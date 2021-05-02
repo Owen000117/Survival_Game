247376a1bd50913636f498f9f1cf946acdfce87a
Owen000117 <owenodea@outlook.com>
2021-05-02 18:51:27 +0100

    Interaction
    - Added a way to interact with the objects by making a main class that can be used to set the basics for the rest of the intractable objects. 
    Added a way for the player to focus on the objects.
    - Added  a point were the player can interact with a cert ion side of an object.

--- Assets/RPGPP_LT/Scripts/PlayerMotor.cs
+++ Assets/RPGPP_LT/Scripts/PlayerMotor.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour
 {
+    Transform target;
     NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
@@ -14,9 +15,36 @@ public class PlayerMotor : MonoBehaviour
         
     }
 
+    private void Update()
+    {
+        if(target != null)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
     // Update is called once per frame
     public void MoveToPoint(Vector3 point)
     {
         agent.SetDestination(point);
     }    
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        agent.stoppingDistance = newTarget.radius * .8f;
+        agent.updateRotation = false;
+        target = newTarget.interactionTransform;
+    }
+    public void StopFollowTarget()
+    {
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true; 
+        target = null;
+    }
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotataion = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotataion, Time.deltaTime * 5f);
+    }
 }
